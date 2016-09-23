675d824d4813fe575770ec4fe4c30e30a6dddb5e
Greg Dorrell <grdorrell@hotels.com>
2016-09-23 16:39:06 +0100

    Dependency inject the services to the controller, and add a new endpoint for "hotels near London"

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,22 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import play.api.libs.json.Json
 import play.api.mvc._
+import services.{GeoService, HotelCatalogueService, HotelFinderService}
+import play.api.libs.json.Writes._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject() (
+  geoService: GeoService,
+  finderService: HotelFinderService,
+  catalogueService: HotelCatalogueService
+) extends Controller {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -19,6 +26,18 @@ class HomeController @Inject() extends Controller {
    */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def hotelsNearLondon = Action{
+    Ok{
+      Json.toJson(
+        for {
+          coordinates <- geoService.lookupCoordinates("London").toSeq
+          (hotelId, _) <- finderService.findHotels(coordinates, 2)
+          hotel <- catalogueService.lookupHotel(hotelId)
+        } yield hotel
+      )
+    }
   }
 
 }
