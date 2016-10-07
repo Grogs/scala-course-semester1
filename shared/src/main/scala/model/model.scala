359ee855b3d175c213cebfc6035db0e88d2f11f5
Greg Dorrell <grdorrell@hotels.com>
2016-10-07 16:57:40 +0100

    Return small and large images for hotels

--- shared/src/main/scala/model/model.scala
+++ shared/src/main/scala/model/model.scala
@@ -2,5 +2,5 @@ package model
 
 case class Coordinates(lat: Double, long: Double)
 
-case class Hotel(id: Long, name: String, coordinates: Coordinates, images: Seq[String])
+case class Hotel(id: Long, name: String, coordinates: Coordinates, smallImages: Seq[String], largeImages: Seq[String])
 
