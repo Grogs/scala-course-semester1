107daeafb46c44ad8c95c1eda02791a8e9445766
Greg Dorrell <grdorrell@hotels.com>
2016-10-07 17:05:22 +0100

    implement some client code, we're going with the progressive enhancement approach. use the autocomplete plugin for the destination input. and use "PJAX" to live update the listings when the destination or distance are changed, without a reload

--- client/src/main/scala/HelloWorld.scala
+++ client/src/main/scala/HelloWorld.scala
@@ -1,10 +1,37 @@
-import scala.scalajs.js.JSApp
+import scala.scalajs.js.{JSApp, debugger}
 import scala.scalajs.js.annotation.JSExport
+import org.scalajs.dom._
+import org.scalajs.dom.ext.Ajax
+import org.scalajs.dom.html.{Button, Input}
 
 object HelloWorld extends JSApp {
 
     @JSExport
     override def main(): Unit = {
-        println("Hello World!")
+        println("Application initiated")
+        val destinationInput = document.getElementById("destination").asInstanceOf[Input]
+        val distanceInput = document.getElementById("distance").asInstanceOf[Input]
+
+        new Autocomplete(
+            destinationInput,
+            Seq("London", "Paris", "Bath"),
+            (s) => reload(s, distanceInput.value)
+        )
+
+        distanceInput.onchange = (e: Event) => reload(destinationInput.value, distanceInput.value)
+
+        document.getElementById("load-hotels").asInstanceOf[Button].style.display = "none"
+
+        //Poor man's PJAX
+        import scala.concurrent.ExecutionContext.Implicits.global
+        def reload(destination: String, distance: String) =
+            Ajax.get(s"/hotels?destination=$destination&distance=$distance", responseType = "document").map( xhr =>
+                xhr.responseXML.getElementById("hotels")
+            ).foreach( newListings =>
+                document.getElementById("hotels").innerHTML = newListings.innerHTML
+            )
+
+
     }
+
 }
