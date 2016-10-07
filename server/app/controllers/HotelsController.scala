a1c9f485d11a985169eca4a51cdf471bdd3f98e7
Greg Dorrell <grdorrell@hotels.com>
2016-10-07 17:01:19 +0100

    implement the hotel listing page

--- server/app/controllers/HotelsController.scala
+++ server/app/controllers/HotelsController.scala
@@ -3,11 +3,18 @@ package controllers
 import javax.inject.Inject
 
 import play.api.mvc._
+import services.hotels.HotelsService
 
-class HotelsController @Inject() (webJarAssets: WebJarAssets) extends Controller {
+class HotelsController @Inject() (webJarAssets: WebJarAssets, hotelsService: HotelsService) extends Controller {
 
-  def landingPage() = Action(
-    Ok(views.html.landingPage(webJarAssets))
-  )
+  def landingPage(destination: Option[String], distance: Option[Float]) = Action{
+
+    val hotels = for {
+      dest <- destination
+      dist <- distance
+    } yield hotelsService.search(dest, dist)
+
+    Ok(views.html.landingPage(webJarAssets)(destination, distance, hotels))
+  }
 
 }
