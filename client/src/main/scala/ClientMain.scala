0d96920fb60062afad9955d84da431a843ac1741
Greg Dorrell <grdorrell@hotels.com>
2016-10-10 13:08:38 +0100

    End of Lesson 3. We have a page that lists hotels, and a form to choose the destination and distance to list hotels for. We started writing client code to pick up changes to the destinaiton and distance, which we will build on in later lessons.

--- client/src/main/scala/ClientMain.scala
+++ client/src/main/scala/ClientMain.scala
@@ -1,10 +1,23 @@
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
+import org.scalajs.dom.{Event, console, document}
+import org.scalajs.dom.html.Input
 
 object ClientMain extends JSApp {
 
     @JSExport
     override def main(): Unit = {
         println("Hello World!")
+        val destinationInput = document.getElementById("destination").asInstanceOf[Input]
+        val distanceInput = document.getElementById("distance").asInstanceOf[Input]
+
+        def getDestination() = destinationInput.value
+        def getDistance() = distanceInput.value
+
+        def reload() = console.log(getDestination(), getDistance())
+
+        destinationInput.onkeydown = (e: Event) => reload()
+        distanceInput.onkeydown = (e: Event) => reload()
+
     }
 }
--- server/app/controllers/HotelsController.scala
+++ server/app/controllers/HotelsController.scala
@@ -3,11 +3,18 @@ package controllers
 import javax.inject.Inject
 
 import play.api.mvc._
+import services.hotels.HotelsService
 
-class HotelsController @Inject() (webJarAssets: WebJarAssets) extends Controller {
+class HotelsController @Inject() (webJarAssets: WebJarAssets, hotelsService: HotelsService) extends Controller {
 
-  def hotelListings() = Action{
-    Ok(views.html.hotelListings(webJarAssets))
+  def hotelListings(destinationOpt: Option[String], distanceOpt: Option[String]) = Action{
+
+    val hotels = for {
+      destination <- destinationOpt
+      distance <- distanceOpt
+    } yield hotelsService.search(destination, distance.toDouble)
+
+    Ok(views.html.hotelListings(webJarAssets)(hotels))
   }
 
 }
