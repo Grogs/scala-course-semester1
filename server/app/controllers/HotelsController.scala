6caeafbf47c585cb6e1b3cac3644ae45cc90f0f4
Greg Dorrell <grdorrell@hotels.com>
2016-10-10 11:34:39 +0100

    Remove HotelService from HotelsController so it's more of a skeleton

--- server/app/controllers/HotelsController.scala
+++ server/app/controllers/HotelsController.scala
@@ -5,15 +5,9 @@ import javax.inject.Inject
 import play.api.mvc._
 import services.hotels.HotelsService
 
-class HotelsController @Inject() (webJarAssets: WebJarAssets, hotelsService: HotelsService) extends Controller {
+class HotelsController @Inject() (webJarAssets: WebJarAssets) extends Controller {
 
-  def hotelListings(destination: Option[String], distance: Option[Float]) = Action{
-
-    val hotels = for {
-      dest <- destination
-      dist <- distance
-    } yield hotelsService.search(dest, dist)
-
+  def hotelListings() = Action{
     Ok(views.html.hotelListings(webJarAssets))
   }
 
