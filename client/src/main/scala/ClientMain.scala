93718d0cb8591bba97a89ece0e68485d4e56aed2
Greg Dorrell <Greg@Dorrell.me>
2016-10-17 00:27:20 +0100

    Use pushState for real PJAX

--- client/src/main/scala/ClientMain.scala
+++ client/src/main/scala/ClientMain.scala
@@ -1,9 +1,12 @@
-import scala.scalajs.js.{JSApp, debugger}
-import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom._
+import org.scalajs.dom.document.location
 import org.scalajs.dom.ext.Ajax
 import org.scalajs.dom.html.{Button, Input}
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.{Dynamic, JSApp}
 
 object ClientMain extends JSApp {
 
@@ -18,20 +21,33 @@ object ClientMain extends JSApp {
         new Autocomplete(
             destinationInput,
             Seq("London", "Paris", "Bath"),
-            s => reload(s, distanceInput.value)
+            reload(_, distanceInput.value)
         )
 
         distanceInput.onchange = (e: Event) => reload(destinationInput.value, distanceInput.value)
+        window.onpopstate = (e: PopStateEvent) => {
+            val state = e.state.asInstanceOf[State]
+            reload(state.destination, state.distance, pushState = false)
+        }
         loadButton.style.display = "none"
 
         //Poor man's PJAX
-        def reload(destination: String, distance: String) =
+        def reload(destination: String, distance: String, pushState: Boolean = true) =
             Ajax.get(s"/hotels?destination=$destination&distance=$distance", responseType = "document").map( xhr =>
                 xhr.responseXML.getElementById("hotels")
-            ).foreach( newListings =>
+            ).foreach { newListings =>
+                if (pushState) {
+                    val path = location.pathname + s"?destination=$destination&distance=$distance"
+                    val state = Dynamic.literal(destination = destination, distance = distance)
+                    window.history.pushState(state, "", path)
+                }
                 hotelTable.innerHTML = newListings.innerHTML
-            )
+            }
 
     }
 
+    trait State extends js.Any {
+        val destination: String
+        val distance: String
+    }
 }
