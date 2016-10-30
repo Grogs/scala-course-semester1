7768f6404a8dc9e7fd79769238d16d0713f5b6f5
Greg Dorrell <Greg@Dorrell.me>
2016-10-30 23:23:58 +0000

    Fixup imports

--- client/src/main/scala/ClientMain.scala
+++ client/src/main/scala/ClientMain.scala
@@ -1,30 +1,18 @@
-import org.scalajs.dom.ext.Ajax
-
-import scala.scalajs.js.{Dynamic, JSApp}
-import scala.scalajs.js.annotation.{JSExport, ScalaJSDefined}
-import org.scalajs.dom._
-import org.scalajs.dom.html.{Button, Div, Input}
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.scalajs.js
-import scala.scalajs.js.Dynamic.literal
-import services.hotels._
 import autowire._
-import model.Hotel
+import google.maps.InfoWindowOptions
+import model.{Coordinates, Hotel}
+import org.scalajs.dom.document.location
+import org.scalajs.dom.html.{Button, Input}
+import org.scalajs.dom.{raw => _, _}
+import org.scalajs.jquery.{JQueryEventObject, jQuery}
+import services.hotels.HotelsServiceApi
 import views.TableView
 
-import scala.concurrent.Future
-import org.scalajs.dom._
-import org.scalajs.dom.document.location
-import org.scalajs.dom.ext.Ajax
-import org.scalajs.dom.html.{Button, Input}
-import services.hotels.HotelsServiceApi
-
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
 import scala.scalajs.js.annotation.{JSExport, ScalaJSDefined}
 import scala.scalajs.js.{Dynamic, JSApp}
 import scalatags.Text.all._
-import autowire._
 
 object ClientMain extends JSApp {
 
@@ -35,14 +23,15 @@ object ClientMain extends JSApp {
         val distanceInput = document.getElementById("distance").asInstanceOf[Input]
         val loadButton = document.getElementById("load-hotels").asInstanceOf[Button]
         val hotelTable = document.getElementById("hotels")
+        val showMapButton = document.getElementById("show-map").asInstanceOf[Button]
 
         loadButton.style.display = "none"
 
         def getDestination = destinationInput.value
-        def getDistance = distanceInput.value
+        def getDistance = distanceInput.value.toDouble
 
-        def reload(destination: String = getDestination, distance: String = getDistance, pushState: Boolean = true) = {
-          Client[HotelsServiceApi].search(destination, distance.toDouble).call().foreach { hotels =>
+        def reload(destination: String = getDestination, distance: Double = getDistance, pushState: Boolean = true) = {
+          Client[HotelsServiceApi].search(destination, distance).call().foreach { hotels =>
             if (hotels.nonEmpty && pushState) {
               val path = location.pathname + s"?destination=$destination&distance=$distance"
               val state = Dynamic.literal(destination = destination, distance = distance)
@@ -55,6 +44,16 @@ object ClientMain extends JSApp {
         destinationInput.onkeyup = (e: Event) => reload()
         distanceInput.onkeyup = (e: Event) => reload()
         distanceInput.onchange = (e: Event) => reload()
+
+        // From http://getbootstrap.com/javascript/#modals-events
+        jQuery("#mapModal").on("shown.bs.modal",
+            (_: JQueryEventObject) => {
+                Client[HotelsServiceApi].search(getDestination, getDistance).call().foreach { hotels =>
+                    document.getElementById("mapModalLabel").innerHTML = s"Hotels within ${getDistance}km of $getDestination"
+                    renderMap(document.getElementById("map"), hotels)
+                }
+            }
+        )
 
         new Autocomplete(
             destinationInput,
@@ -71,6 +70,60 @@ object ClientMain extends JSApp {
     @ScalaJSDefined
     trait State extends js.Any {
         val destination: String
-        val distance: String
+        val distance: Double
     }
+
+    def renderMap(target: Element, hotels: Seq[Hotel]) = {
+
+        val opts = google.maps.MapOptions(
+            center = new google.maps.LatLng(50, 0),
+            zoom = 11
+        )
+
+        val gmap = new google.maps.Map(target, opts)
+
+        val point =
+            for {
+                hotel <- hotels
+                Coordinates(lat, long) = hotel.coordinates
+                latLng = new google.maps.LatLng(lat, long)
+            } yield {
+
+              val marker = new google.maps.Marker(google.maps.MarkerOptions(
+                  position = latLng,
+                  map = gmap,
+                  title = hotel.name
+              ))
+
+              val infoWindow = new google.maps.InfoWindow(
+                  InfoWindowOptions( content =
+                    div(
+                        h2(hotel.name),
+                        p(s"Rating: ${hotel.guestRating}/5 (${hotel.numberOfReviews} reviews)"),
+                        raw(hotel.descriptionHtml)
+                    ).render
+                  )
+              )
+
+              marker -> infoWindow
+            }
+
+        val markerBounds = new google.maps.LatLngBounds()
+        var activeInfoWindow = new google.maps.InfoWindow
+
+        for {
+            (marker, infoWindow) <- point
+        } yield {
+            marker.addListener("click", (_:js.Any) => {
+                activeInfoWindow.close()
+                activeInfoWindow = infoWindow
+                infoWindow.open(gmap, marker)
+            })
+            markerBounds.extend(marker.getPosition())
+        }
+
+        gmap.fitBounds(markerBounds)
+
+    }
+
 }
